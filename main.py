2127e1ca7f62871d6323093e533255b282c60aaa
Solene34 <issartel.solene@gmail.com>
2020-11-24 19:42:17 +0100

    Change formatting

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from pandas import *
 
 
-if __name__ == '__main__':
-    df = pandas.read_csv("data/donnees_data_science.csv", low_memory=False, header=0)
-    df2 = df.head(30)
-    df2 = df2[['From', 'To', 'Subject', 'X-cc', 'content']]
+def formate_datas(csv_file):
+    df = pandas.read_csv(csv_file, low_memory=False, header=0)
+    df = df[['From', 'To', 'Subject', 'X-cc', 'content']]
 
     unwanted_ids = []
-    for index, row in df2.iterrows():
-        if len(row['content']) == 0 \
-                and len(row['Subject']) == 0 \
-                and (len(row['X-cc']) or row['X-cc'].startswith('---------------------- Forwarded by')) == 0:
-            unwanted_ids.append(index)
+    for index, row in df.iterrows():
+        if isinstance(row['content'], float):
+                if isinstance(row['content'], float) :
+                    if isinstance(row['X-cc'], float):
+                        unwanted_ids.append(index)
+                    elif row['X-cc'].startswith('---------------------- Forwarded by'):
+                        unwanted_ids.append(index)
         elif row['content'].startswith('---------------------- Forwarded by'):
             unwanted_ids.append(index)
 
-    print(unwanted_ids)
-    new_df = df2.drop(unwanted_ids)
-    print(new_df)
+    new_df = df.drop(unwanted_ids)
+    return (new_df)
+
+
+if __name__ == '__main__':
+    df = formate_datas("data/donnees_data_science.csv")
+    df.to_csv("data/formatted_data.csv")
