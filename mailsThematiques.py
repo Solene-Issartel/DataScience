5a6191d31ce6e6ff13a8ab5f86ab6e4f225c2430
RAYMOND Luc <luc_ray@hotmail.com>
2020-12-15 10:07:17 +0100

    Fichier permettant d'associer les thématiques aux mails

---
+++ mailsThematiques.py
@@ -0,0 +1,75 @@
+from pandas import *
+import numpy as np
+import time
+
+# Récupérer les mots associés à la thématique
+def retrieve_associated_words(stringToChange):
+    wordsAssociated = stringToChange
+    wordsAssociated = wordsAssociated.replace("[", "")
+    wordsAssociated = wordsAssociated.replace("]", "")
+    wordsAssociated = wordsAssociated.replace(",", "")
+    wordsAssociated = wordsAssociated.replace("'", "")
+    wordsAssociated = wordsAssociated.split(" ")
+    return wordsAssociated
+
+# Selon son sujet
+def associate_to_mails():
+    start_time = time.time()
+    df = pandas.read_csv("data/formatted_data.csv", low_memory=False, header=0)
+    df = df[['Subject']].dropna()
+    df1 = pandas.read_csv("data/clean_thematiques.csv", low_memory=False, header=0)
+    columns = ["IDmail"]
+    # Une ligne vide, qui va ensuite prendre les informations du mail
+    emptyRow = [0]
+    # On récupère les différentes thématiques
+    for thematiques in df1["mainThematique"]:
+        emptyRow.append(0)
+        columns.append(thematiques)
+    # les mails avec leurs thématiques
+    mailsThematiques = pandas.DataFrame(columns=columns)
+    nbMail = 0
+    # Pour chaque mail
+    for mailTraite in df.iterrows():
+        nbMail += 1
+        print(nbMail)
+        mailTraite[1].str.lower()
+        # On crée une nouvelle ligne
+        newMailRow = emptyRow
+        # On en fait un dataframe comprenant toutes les colonnes voulues
+        newMailRow = pandas.DataFrame(data=np.array([newMailRow]), columns=columns)
+        # On indique quel mail on traite, on récupère son id
+        newMailRow["IDmail"] = mailTraite[0]
+        # Pour chaque thématique existante
+        for rowThemes in df1.iterrows():
+            # La thématique que l'on traite
+            thematique = rowThemes[1].mainThematique
+            # Le sujet dans lequel on veut vérifier la thématique
+            subject = mailTraite[1].Subject
+            # Les mots associés à la thématique
+            wordsAssociated = retrieve_associated_words(rowThemes[1].wordsAssociated)
+            # On regarde si la thématique est présente
+            if(thematique in subject):
+                print(thematique)
+                newMailRow[thematique] = 1
+            # Sinon on regarde si un des mots proches est présents
+            else:
+                exist = False
+                # On commence à 1 car le premier mot est la thématique
+                count = 1
+                while not exist and count < len(wordsAssociated):
+                    # Le mot est présent ?
+                    if (wordsAssociated[count] in subject):
+                        print(thematique)
+                        # Oui, on indique que le mail est de cette thématique
+                        exist = True
+                        newMailRow[thematique] = 1
+                    count += 1
+            # Puis on ajoute notre mail avec ses thématiques dans le dataframe
+        mailsThematiques = pandas.concat([mailsThematiques, newMailRow], ignore_index=True)
+    # On a fini d'attribuer les différentes thématiques aux mails
+    mailsThematiques.to_csv("data/mails_thematiques.csv")
+    stop_time = time.time()
+    print("\nTemps de calcul = %f secondes" % (stop_time - start_time))
+
+if __name__ == '__main__':
+    associate_to_mails()
--- thematique.py
+++ thematique.py
@@ -3,34 +3,58 @@ from nltk.corpus import wordnet
 from pandas import *
 import time
 
+# Un peu comme une méthode k-means
 def create_thematiques(col,df):
     start_time = time.time()
-    # La liste qui contiendra les thématiques avec leurs mots associées
+    # La liste qui contiendra les thématiques avec tous leurs mots associées
     thematiques = []
+    # Les thématiques avec seulement les mots apparaissant de bases en mots associés
+    realThematiques = []
     # Pour chaque mot récupéré dans les subjects
     for motBrut in df[col]:
         # On enlève les mots au pluriel
-        mot =  WordNetLemmatizer().lemmatize(motBrut)
+        mot = WordNetLemmatizer().lemmatize(motBrut)
+        # On récupère les mots proches du mot actuel
+        closeWords = wordnet.synsets(mot)
         exist = False
         count = 0
         # On regarde si on a déjà eu ce mot dans les mots proches précédents
-        while (count < len(thematiques) and exist == False):
+        while (count < len(thematiques) and not exist):
             if (mot in thematiques[count]):
                 exist = True
             else:
+                # Tester la similarité du mot avec les thématiques déjà trouvées
+                # seulement si le mot existe dans la librairie wordnet
+                if (len(closeWords) != 0):
+                    for motsTestés in thematiques[count]:
+                        # On récupère les synonymes d'un des mots
+                        synsetMT = wordnet.synsets(motsTestés)
+                        # Seulement si le mot existe dans wordnet
+                        if(len(synsetMT) != 0):
+                            # On prend juste le premier
+                            synsetMT = synsetMT[0]
+                            similarity = (closeWords[0]).wup_similarity(synsetMT)
+                            if(similarity == None):
+                                similarity = 0
+                            # On regarde s'ils sont similaires
+                            if(similarity >= 0.65):
+                                exist = True
+            # On passe à la ligne suivante si on n'a pas trouvé de mot similaire
+            if (not exist):
                 count += 1
-        # On récupère les mots en commun si on ne l'a pas déjà traité
-        closeWords = wordnet.synsets(mot)
         if (exist):
-            # Mot déjà traité, on les rajoute dans la bonne case
+            # Mot déjà traité, on le rajoute dans la bonne case
             for cws in closeWords:
                 # On regarde si la liste du mot proche contient le mot cible (pour filtrer)
                 if mot in cws.lemma_names():
                     # On ajoute chacun des mots dans la bonne case de thématiques
                     for w in cws.lemma_names():
                         thematiques[count].append(w)
+                        if(w == mot):
+                         realThematiques[count].append(motBrut)
         else:
             # Nouvelle thématique
+            newRealThematique = []
             newThematique = []
             for cws in closeWords:
                 # On regarde si la liste du mot proche contient le mot cible (pour filtrer)
@@ -38,20 +62,29 @@ def create_thematiques(col,df):
                     # On ajoute chaque mot dans la nouvelle thématique
                     for w in cws.lemma_names():
                         newThematique.append(w)
+                        if(w == mot):
+                            newRealThematique.append(motBrut)
             # Mot inconnu
             if len(newThematique) == 0:
                 newThematique.append(mot)
+                newRealThematique.append(motBrut)
             # Nouvelle thématique prête, on l'ajoute dans la liste
             thematiques.append(newThematique)
+            realThematiques.append(newRealThematique)
     stop_time = time.time()
     print("\nTemps de calcul = %f secondes" % (stop_time - start_time))
-    print(len(thematiques))
-    print(len(df[col]))
-    return thematiques
+    print("Nous passons de %d à %d thématiques " % (len(df[col]), len(realThematiques)))
+    return realThematiques
+
+#def remove_useless_synsets(thematiques):
+    # On ne veut pas garder les synonymes n'apparaîssant pas réellement dans les mots
+    # associés aux thématiques
+    # for index, row in thematiques.iterrows():
+
 
 def extractThematique(thematiques):
     for i in range(0,len(thematiques)):
-        # On récupère la thématique en dataframe
+        # On récupère les thématiques en dataframe
         df = DataFrame(thematiques[i], columns=["words"])
         # On rajoute un compteur sur chaque mot
         df["count"] = 1
@@ -69,15 +102,11 @@ def extractThematique(thematiques):
 
 if __name__ == '__main__':
     df1 = pandas.read_csv("data/map_reduced_subject.csv", low_memory=False, header=0)
-    df1 = df1.dropna()
-    df2 = pandas.read_csv("data/map_reduced_content.csv", low_memory=False, header=0)
-    df2 = df2.dropna()
 
     thematiques1 = create_thematiques("Subject",df1)
     thematiques1 = extractThematique(thematiques1)
-
-    #thematiques2 = create_thematiques("content",df2)
-    #thematiques2 = extractThematique(thematiques2)
+    thematiques1 = pandas.DataFrame(thematiques1, columns=["mainThematique","wordsAssociated"])
+    thematiques1.to_csv("data/clean_thematiques.csv")
 
     for theme in thematiques1:
         print(theme)
